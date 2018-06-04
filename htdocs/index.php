d1b488825d0fcacca89eb1532f325a01c0f4d770
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2018-06-03 23:03:39 -0300

    Updated post variables

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,5 +2,8 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $cpf = $_POST["cpf"];
-$mensagem - $_POST[""];
+$password = $_POST["password"];
+$passwordRetype = $_POST["passwordRetype"];
+$gender = $_POST["gender"];
+$acceptedTerms - $_POST["acceptedTerms"];
 ?>
